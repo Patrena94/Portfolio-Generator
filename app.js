c59c12319449b78c83b2cf3fa680cf6cdd6ebd33
Vashti <patrena@sbcglobal.net>
2021-07-20 19:52:30 -0700

    added node/modules and questions

--- app.js
+++ app.js
@@ -34,31 +34,41 @@ const { fstat } = require("fs");
 // const generatePage = (userName, githubName) =>'Name: ${userName}, Github: ${githubName}';
 // console.log(generatePage('Patrena A. Smith','Patrena94'));
 
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
-console.log(process.argv)
-const generatePage = (userName, githubName) => { 
-return `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-</head>
-<body>
-    <h1>${name}</h1>
-    <h2><a href="https://github.com/${github}">Github</a></h2>
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('index.html', generatePage("Patrena Smith", "Patrena94"), err => {
+//     if(err) throw err;
+//     console.log('portfolio complete! Check out index.html to see the output!');
+// });
+
+// const generatePage = (userName, githubName) => { 
+// return `
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+// <meta charset="UTF-8">
+// <meta name="viewport" content="width=device-width, initial-scale=1">
+// <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//     <title>Portfolio Demo</title>
+// </head>
+// <body>
+//     <h1>${name}</h1>
+//     <h2><a href="https://github.com/${github}">Github</a></h2>
     
-// </body>
-// <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-</html>
-`;
-};
-fs.writeFile('index.html', generatePage("Patrena Smith", "Patrena94"), err => {
-    if(err) throw err;
-    console.log('portfolio complete! Check out index.html to see the output!');
-});
+// // </body>
+// // <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+// </html>
+// `;
+// };
+inquirer
+.prompt([
+    {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+}
+])
+.then(answers => console.log(answers));
